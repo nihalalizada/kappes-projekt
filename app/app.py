c6fcb3f47732ddbc1d6970087fbcf6e0928ba4c8
Nihal Alizada <nihalhaleem@gmail.com>
2022-12-11 18:22:33 +0100

    added post

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
 from typing import List, Dict
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 import json
 
 app = Flask(__name__)
-
 
 def users() -> List[Dict]:
     config = {
@@ -28,11 +27,20 @@ def users() -> List[Dict]:
 def index() -> dict:
     return {'users': users()}
 
-@app.route('/index.html')
+@app.route('/index.html', methods=['POST', 'GET'])
 def home():
-    title = 'Home'
-    return render_template('index.html', title = title)
-
+    if request.method == "POST":
+       username = request.form["username"]
+       password = request.form["password"]
+  
+       if username =='test' & password == 'test':
+         return render_template('/loggedIn.html')
+       
+    return render_template('index.html', error=True)
+    
+@app.route('/loggedIn.html', methods=['POST', 'GET'])
+def loggedIn():
+    return render_template('loggedIn.html', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
